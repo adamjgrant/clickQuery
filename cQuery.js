b86445b52f70ab14c17c10caa3ea2c165a533b41
Adam Kochanowicz <ajkochanowicz@gmail.com>
2014-08-12 17:47:51 -0400

    3 lines

--- cQuery.js
+++ cQuery.js
@@ -1,3 +1,3 @@
 var o=function(s){var m=this;m.length=0;m.splice=function(){};m.t=document.querySelectorAll(s);for(i=0;i<m.t.length;i++)m.push(m.t[i]);return m;}
-r=o.prototype,p= Array.prototype,r.length = 0,r.push = p.push,r.splice = p.splice;r.click = function(fn){for(i=0,m=this;i<m.t.length;i++){var z=m.t[i],b=z.cloneNode(true);z.parentNode.replaceChild(b,z);b.addEventListener('click',fn);}return m;}
+r=o.prototype,p= Array.prototype,r.length=0,r.push=p.push,r.splice=p.splice;r.click=function(fn){for(i=0,m=this;i<m.t.length;i++){var z=m.t[i],b=z.cloneNode(true);z.parentNode.replaceChild(b,z);b.addEventListener('click',fn);}return m;}
 var $=function(s){return new o(s);}
